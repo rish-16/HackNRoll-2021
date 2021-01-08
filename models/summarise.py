c048ff2ae4886b4b54eda31421228c83e608d1b2
rish-16 <mail.rishabh.anand@gmail.com>
2021-01-08 14:59:32 +0800

    Add BERTSummariser object with samples

--- models/summarise.py
+++ models/summarise.py
@@ -1,20 +1,28 @@
 from summarizer import Summarizer
 from pprint import pprint
 
-model = Summarizer()
+class BERTSummariser:
+    def __init__(self):
+        self.model = Summarizer()
+        
+    def predict(self, body):
+        return self.model(body, ratio=0.4)
 
+bert = BERTSummariser()
 data = {}
 
-with open("transcript.txt") as f:
+with open("text/transcript3.txt") as f:
     body = f.read().strip().split("\n")
     body = [body[i].strip().split(" | ") for i in range(len(body))]
     
 for i in range(len(body)):
     data[body[i][1]] = body[i][0]
     
-text = "".join(list(data.values()))
+text = ". ".join(list(data.keys()))
+print (text)
 
-preds = model(text, ratio=0.4)
+preds = bert.predict(text)
+print (preds)
 
-with open("result.txt", "a") as f:
+with open("res/result3.txt", "a") as f:
     f.write("".join(preds))
