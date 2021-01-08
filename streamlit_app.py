3ddca05eb45e835f7280856b75b2350234442724
Abhishekbhagwat <abhishek.bhagwat1999@gmail.com>
2021-01-09 04:15:14 +0800

    fix transcript generation logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,32 +6,35 @@ from models.bert_sum import BERTSummariser
 from pathlib import Path
 from streamlit.components.v1 import declare_component
 from streamlit_player import st_player
+from fuzzywuzzy import fuzz
+
 
 def init_model():
     bert = BERTSummariser()
     return bert
 
-# @st.cache()
 def preprocess_data(url):
     hash_lookup = {}
     df = get_data(url)
 
-    sents = df['text']
-    text = ""
-    prev = ""
-    prev_row = None
+    raw_text = ""
+    full_sent = ""
+    first_timestamp = None
 
     for idx, row in df.iterrows():
-        if "." in row['text']:
-            prev += " " + row['text']
-            hash_lookup[prev] = prev_row
-            prev = ""
-            prev_row = {row['start'], row['end'], row['duration']}
+        sent = row['text'].strip()
+
+        if sent[-1] == "." or sent[-1] == "?" or sent[-1] == "!":
+            full_sent += sent[:-1] + "."
+            hash_lookup[full_sent] = first_timestamp
+            # do something with full sentence and timestamp
+            full_sent = ""
         else:
-            prev += " " + row['text']
-            prev_row = {row['start'], row['end'], row['duration']}
-            
-    text = "".join(list(hash_lookup.keys()))
+            full_sent += sent + " "
+        
+        first_timestamp = [row['start'], row['end'], row['duration']]
+    # combine to form input paragraph
+    text = " ".join(list(hash_lookup.keys()))
     return text
 
 
@@ -48,7 +51,6 @@ url = st.text_input('Enter the URL of your video')
 if st.button('Pimp my video!'):
     with st.spinner("Please wait till we make your mind go brr........"):
         bert_model = init_model()
-        bert_model = BERTSummariser()
         transcript_text = preprocess_data(url)
         transcript_text_summary = bert_model.predict(transcript_text)
         st_player(url)
